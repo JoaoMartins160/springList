f127f7e0dceef1f63a969c8e956bc9f75c77b4f9
João Pedro <83091138+JoaoMartins160@users.noreply.github.com>
2023-07-13 17:03:19 -0300

    Project Creation

---
+++ src/main/java/com/intesivospringsuperior/demo/IntensivoSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.intesivospringsuperior.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivoSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivoSuperiorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intesivospringsuperior/demo/IntensivoSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intesivospringsuperior.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
